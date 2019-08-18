bb96b805be5ce950fad8b83adce9db750fb93978
ayelenperezdemendiola <a.perezdemendiola@gmail.com>
2019-08-18 18:05:04 +0200

    beggining of first part of javascript

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-console.log('>> Ready :)');
+const inputs = document.querySelectorAll ('.input');
+const button = document.querySelector ('.start__button');
+let gameValue='';
+const api = 'https://raw.githubusercontent.com/Adalab/cards-data/master/';
+
+function choosedValue (event){
+  gameValue = event.currentTarget.value;
+  console.log (gameValue);
+}
+
+for (const input of inputs){
+  input.addEventListener ('click', choosedValue);
+}
+
+function start(){
+  const ENDPOINT = api + gameValue + '.json';
+  fetch(ENDPOINT)
+    .then(response => response.json())
+    .then(data => {
+      console.log (data[0].image);
+    }
+)
+}
+
+
+
+//hacer fetch
+//hacer .then
+
+
+button.addEventListener('click', start);
+
+// Al hacer clic sobre el botón de 'Comenzar', nuestra aplicación debe recoger el valor del tamaño de la partida y conectarse a un API que devuelve un listado de cartas. La URL del API es https://raw.githubusercontent.com/Adalab/cards-data/master/:NUMERO.json, donde :NUMERO puede tomar el valor de 4, 6 u 8. Por ejemplo, para pedir 6 cartas usariamos https://raw.githubusercontent.com/Adalab/cards‒data/master/6.json
